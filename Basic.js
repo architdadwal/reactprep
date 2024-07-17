782116992854bdc18ba6877d22a32ec4d286a60c
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2024-07-17 23:20:33 +0530

    Update Basic.js

--- Basic.js
+++ Basic.js
@@ -1,3 +1,13 @@
-What is React ?
+Q1. What is React ?
 
-ANS >>  React is a JS Library used to create ingle page applications
+Ans : React is a JS Library used to create single page applications.
+
+
+Q2.  What are props ? 
+
+  Ans: Props is a special keyword in React that stands for properties and is used for passing data from one component to another. Data with props are passed in a unidirectional flow from parent to child.
+  Basically arguments passed to react components.
+ const myElement = <Car  brand ="Ford" />
+  function Car(props) {
+  return <h2>I am a { props.brand }!</h2>;
+}
